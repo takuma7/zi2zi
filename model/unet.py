a9b70d9899a6118fde1671f69a245a669a07f960
Takuma YOSHITANI <takuma.7@gmail.com>
2017-04-26 01:44:20 +0900

    python2 to python3

--- model/unet.py
+++ model/unet.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-from __future__ import absolute_import
-
 import tensorflow as tf
 import numpy as np
 import scipy.misc as misc
--- model/utils.py
+++ model/utils.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-from __future__ import absolute_import
-
 import os
 import glob
 
 import imageio
 import scipy.misc as misc
 import numpy as np
-from cStringIO import StringIO
+from io import BytesIO as StringIO
 
 
 def pad_seq(seq, batch_size):
